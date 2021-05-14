cd0daeef71a0e4944c0da0cbe9eadeb1f9701995
tfpresentes <83709963+tfpresentes@users.noreply.github.com>
2021-05-14 16:59:39 -0300

    blank page only logo

---
+++ app.js
@@ -0,0 +1,10 @@
+const app = Vue.createApp({
+    //template: '<h2>I am the template</h2>'
+    data() {
+        return{
+            title: 'the final empire'
+        }
+    }
+})
+
+app.mount('#app')
